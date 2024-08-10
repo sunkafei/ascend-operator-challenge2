abb813e9cf8c37805cd123b19908c8ed5142e778
sunmingzhi <531483935@qq.com>
2024-08-10 07:28:51 +0000

    Add DepthToSpace

---
+++ DepthToSpace/op_host/depth_to_space_tiling.h
@@ -0,0 +1,18 @@
+
+#include "register/tilingdata_base.h"
+
+namespace optiling {
+BEGIN_TILING_DATA_DEF(DepthToSpaceTilingData)
+    TILING_DATA_FIELD_DEF(uint32_t, totalLength);
+    TILING_DATA_FIELD_DEF(uint32_t, tileNum);
+    TILING_DATA_FIELD_DEF(uint32_t, ALIGN_NUM);
+    TILING_DATA_FIELD_DEF(uint32_t, tiling_size);
+    TILING_DATA_FIELD_DEF(uint32_t, block_size);
+    TILING_DATA_FIELD_DEF(uint32_t, aivNum);
+    TILING_DATA_FIELD_DEF(uint32_t, core_size);
+    TILING_DATA_FIELD_DEF(uint32_t, core_remain);
+    TILING_DATA_FIELD_DEF(uint32_t, lastdim);
+END_TILING_DATA_DEF;
+
+REGISTER_TILING_DATA_CLASS(DepthToSpace, DepthToSpaceTilingData)
+}
